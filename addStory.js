b6ef109911646d958d6fde8e1c2a68929df6293b
Scodia619 <billyjoe2701@gmail.com>
2023-08-28 00:32:00 +0100

    V1.5.2
    Authentication before uploading

--- addStory.js
+++ addStory.js
@@ -1,46 +1,86 @@
 function submitStory() {
     // This is where you can handle the form submission
     // For example, you can get the values of the inputs and perform actions
+    if(sessionStorage.getItem("name")){
+      createHash();
 
-    createHash();
+      let currentId = parseInt(sessionStorage.getItem("length"));
+      let newId = currentId + 1;
 
-    let currentId = parseInt(sessionStorage.getItem("length"));
-    let newId = currentId + 1;
-
-    console.log(newId)
-    
-    const storyValue = document.getElementById('story').value;
-    const typeValue = document.getElementById('type').value;
-    
-    fetch("https://urwelsydxf.execute-api.eu-west-1.amazonaws.com/default/postStoryCyberman", {
-        method: "POST",
-        body: JSON.stringify({
-          Story: storyValue,
-          Type: typeValue,
-          id: newId,
-          User: sessionStorage.getItem("user")
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((res) => {
-          if (res.ok) {
-            console.log(res);
-            return res.json();
-          }
-          throw new Error("Request failed!");
+      console.log(newId)
+      
+      const storyValue = document.getElementById('story').value;
+      const typeValue = document.getElementById('type').value;
+      
+      fetch("https://urwelsydxf.execute-api.eu-west-1.amazonaws.com/default/postStoryCyberman", {
+          method: "POST",
+          body: JSON.stringify({
+            Story: storyValue,
+            Type: typeValue,
+            id: newId,
+            User: sessionStorage.getItem("user")
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
         })
-        .then((data) => {
-          //window.location.reload();
-        })
-        .catch((error) => {
-          console.error("There was a problem with the fetch operation:", error);
-        });
+          .then((res) => {
+            if (res.ok) {
+              console.log(res);
+              return res.json();
+            }
+            throw new Error("Request failed!");
+          })
+          .then((data) => {
+            showToast("Story Posted!", "success");
+          })
+          .catch((error) => {
+            console.error("There was a problem with the fetch operation:", error);
+          });
 
-    // Perform actions with the values
-    console.log('Story:', storyValue);
-    console.log('Type:', typeValue);
+      // Perform actions with the values
+      console.log('Story:', storyValue);
+      console.log('Type:', typeValue);
+  }else{
+    showToast("Need To Log In!", "warning")
+  }
+}
+
+function showToast(message, type) {
+  const toastContainer = document.getElementById("toast-container");
+
+  const toast = document.createElement("div");
+  toast.className = `toast align-items-center text-white bg-${type}`;
+  toast.setAttribute("role", "alert");
+  toast.setAttribute("aria-live", "assertive");
+  toast.setAttribute("aria-atomic", "true");
+
+  const toastBody = document.createElement("div");
+  toastBody.className = "d-flex";
+
+  const toastIcon = document.createElement("div");
+  toastIcon.className = "toast-icon me-3";
+  toastIcon.innerHTML = '<i class="bi bi-info-circle"></i>'; // Replace with desired icon
+
+  const toastContent = document.createElement("div");
+  toastContent.className = "toast-content";
+  toastContent.textContent = message;
+
+  const closeButton = document.createElement("button");
+  closeButton.type = "button";
+  closeButton.className = "btn-close btn-close-white me-2 m-auto";
+  closeButton.setAttribute("data-bs-dismiss", "toast");
+  closeButton.setAttribute("aria-label", "Close");
+
+  toastContent.appendChild(closeButton);
+  toastBody.appendChild(toastIcon);
+  toastBody.appendChild(toastContent);
+  toast.appendChild(toastBody);
+
+  toastContainer.appendChild(toast);
+
+  const bsToast = new bootstrap.Toast(toast);
+  bsToast.show();
 }
 
 function generateRandomSalt(length) {
--- gallery.js
+++ gallery.js
@@ -80,6 +80,19 @@ closeButton.addEventListener("click", () => {
   modal.style.display = "none";
 });
 
+modal.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
+
+// Close modal on outside click
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
+
 // Navigate to previous photo
 prevButton.addEventListener("click", () => {
   navigatePhoto("prev");
