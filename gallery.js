825661d1c88f661a194aa36f508653c0dbdd80dd
Scodia619 <billyjoe2701@gmail.com>
2023-08-27 01:03:45 +0100

    v1.4
    Adding Multiple Images at Once

--- gallery.js
+++ gallery.js
@@ -1,55 +1,100 @@
-const container = document.getElementById("image-container")
 const galleryContainer = document.getElementById("gallery-container");
 const modal = document.getElementById("modal");
 const modalImage = document.getElementById("modal-image");
 const closeButton = document.getElementById("close-button");
+const prevButton = document.getElementById("prev-button");
+const nextButton = document.getElementById("next-button");
+let currentPhotoIndex = 0;
+let photosData = [];
 
-
-function getPhotos(){
-
-    fetch('https://iwsiesfgs8.execute-api.eu-west-1.amazonaws.com/default/getAllPhotosCyberman', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-    .then((response) => response.json())
-    .then((data) => {
-        console.log("body", JSON.parse(data.body))
-        const photos = JSON.parse(data.body);
-        displayPhotos(photos)
-    })
-    .catch((error) => {
-        console.error(error);
-    });
+function getPhotos() {
+  fetch('https://iwsiesfgs8.execute-api.eu-west-1.amazonaws.com/default/getAllPhotosCyberman', {
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+  })
+  .then((response) => response.json())
+  .then((data) => {
+      console.log("body", JSON.parse(data.body))
+      photosData = JSON.parse(data.body);
+      displayPhotos(photosData);
+  })
+  .catch((error) => {
+      console.error(error);
+  });
 }
 
-function displayPhotos(photos){
-    photos.forEach((photo) => {
-        const image = document.createElement("img");
-        image.src = photo.image_url.S;
-        image.className = "gallery-image";
-        image.addEventListener("click", () => openModal(photo.image_url.S));
-        galleryContainer.appendChild(image);
-      });
+function displayPhotos(photos) {
+  photos.forEach((photo, index) => {
+    const image = document.createElement("img");
+    image.src = photo.image_url.S;
+    image.className = "gallery-image";
+    image.addEventListener("click", () => openModal(photo.image_url.S, index));
+    galleryContainer.appendChild(image);
+  });
 }
 
-// Open modal with clicked image
 function openModal(imageUrl) {
-    modalImage.src = imageUrl;
-    modal.style.display = "flex";
-  }
-  
-  // Close modal
-  closeButton.addEventListener("click", () => {
-    modal.style.display = "none";
-  });
-  
-  // Close modal on outside click
-  window.addEventListener("click", (event) => {
-    if (event.target === modal) {
-      modal.style.display = "none";
+  modalImage.src = imageUrl;
+  modal.style.display = "flex";
+
+  // Dynamically adjust modal size based on image dimensions
+  const img = new Image();
+  img.src = imageUrl;
+  img.onload = function () {
+    const aspectRatio = img.width / img.height;
+
+    const maxWidth = window.innerWidth * 0.8; // Adjust as needed
+    const maxHeight = window.innerHeight * 0.8; // Adjust as needed
+
+    let modalWidth = img.width;
+    let modalHeight = img.height;
+
+    if (img.width > maxWidth) {
+      modalWidth = maxWidth;
+      modalHeight = maxWidth / aspectRatio;
     }
-  });
+
+    if (modalHeight > maxHeight) {
+      modalHeight = maxHeight;
+      modalWidth = maxHeight * aspectRatio;
+    }
+
+    modalImage.style.width = `${modalWidth}px`;
+    modalImage.style.height = `${modalHeight}px`;
+  };
+}
+
+function navigatePhoto(direction) {
+  if (direction === "prev") {
+    currentPhotoIndex = (currentPhotoIndex - 1 + photosData.length) % photosData.length;
+  } else if (direction === "next") {
+    currentPhotoIndex = (currentPhotoIndex + 1) % photosData.length;
+  }
+  modalImage.src = photosData[currentPhotoIndex].image_url.S;
+}
+
+// Close modal
+closeButton.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+// Navigate to previous photo
+prevButton.addEventListener("click", () => {
+  navigatePhoto("prev");
+});
+
+// Navigate to next photo
+nextButton.addEventListener("click", () => {
+  navigatePhoto("next");
+});
+
+// Close modal on outside click
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
 
 getPhotos();
